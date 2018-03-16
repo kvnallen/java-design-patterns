9fdecdb9587dfd48b16dba3bfa529c962a70f6f7
Kevin Allen <kevinallenlf@gmail.com>
2018-03-15 21:32:35 -0300

    Added info at singleton

--- src/creational/singleton/App.java
+++ src/creational/singleton/App.java
@@ -3,6 +3,8 @@ package creational.singleton;
 public class App {
 
 	public static void main(String[] args) {
+		System.out.println("If you see the same hashcode, so the singleton was reused!");
+		
 		DataBase.getInstance().query("from world");
 		DataBase.getInstance().query("from world");
 	}
